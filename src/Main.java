3b140c645016c54e25f700118d051a5ec0f53dc1
makone <dobrovanya@gmail.com>
2022-11-03 15:38:28 +0400

    Изменил логику арифметических методов

--- src/Main.java
+++ src/Main.java
@@ -10,7 +10,6 @@ public class Main {
         while (flag) {
             menuPrint();
             flag = menuChoice(scr.nextInt());
-            System.out.println(result);
 
         }
 
@@ -47,122 +46,133 @@ public class Main {
         String[] symbols = operation.split(" ");
         switch (symbols[1]) {
             case "+":
-                result = sum(Integer.parseInt(symbols[0]), Integer.parseInt(symbols[2]));
+                sum(Integer.parseInt(symbols[0]), Integer.parseInt(symbols[2]));
                 break;
             case "-":
-                result = sub(Integer.parseInt(symbols[0]), Integer.parseInt(symbols[2]));
+                sub(Integer.parseInt(symbols[0]), Integer.parseInt(symbols[2]));
                 break;
             case "*":
-                result = mult(Integer.parseInt(symbols[0]), Integer.parseInt(symbols[2]));
+                mult(Integer.parseInt(symbols[0]), Integer.parseInt(symbols[2]));
                 break;
             case "/":
-                result = div(Integer.parseInt(symbols[0]), Integer.parseInt(symbols[2]));
+                div(Integer.parseInt(symbols[0]), Integer.parseInt(symbols[2]));
                 break;
             case "!":
-                result = fact(Integer.parseInt(symbols[0]));
+                System.out.println(fact(Integer.parseInt(symbols[0])));
                 break;
             case "^":
-                result = pow(Integer.parseInt(symbols[0]), Integer.parseInt(symbols[2]));
+                pow(Integer.parseInt(symbols[0]), Integer.parseInt(symbols[2]));
                 break;
             case "?":
-                result = comp(Boolean.parseBoolean(Integer.parseInt(symbols[0]), Boolean.parseBoolean(String.valueOf(Integer.parseInt(symbols[2])))));
+                comp(symbols[0], symbols[2]);
+                break;
+            default:
+                System.out.println("Не известная операция, попробуй еще");
+        }
+    }
+
+    public static void effectiveValue() {
+        Scanner scrNewValue = new Scanner(System.in);
+        System.out.println("Выберите арифметическую операцию и введите второре число: ");
+        System.out.println("Пример: 1 2, где 1 - сложение, 2 - число, которое будем прибавлять.\nВажно, числа вводить через пробел!");
+        menuEffectiveValue();
+        String operation = scrNewValue.nextLine();
+        String[] symbols = operation.split(" ");
+        switch (symbols[0]) {
+            case "1":
+                sum(result, Integer.parseInt(symbols[1]));
+                break;
+            case "2":
+                sub(result, Integer.parseInt(symbols[1]));
+                break;
+            case "3":
+                mult(result, Integer.parseInt(symbols[1]));
+                break;
+            case "4":
+                div(result, Integer.parseInt(symbols[1]));
+                break;
+            case "5":
+                pow(result, Integer.parseInt(symbols[1]));
+                break;
+            case "0":
+                menuPrint();
+                break;
             default:
                 System.out.println("Не известная операция, попробуй еще");
                 result = Integer.parseInt(symbols[0]) + Integer.parseInt(symbols[2]);
         }
+
     }
-        public static void effectiveValue () {
-            Scanner scrNewValue = new Scanner(System.in);
-            System.out.println("Выберите арифметическую операцию и введите второре число: ");
-            System.out.println("Пример: 1 2, где 1 - сложение, 2 - число, которое будем прибавлять.\nВажно, числа вводить через пробел!");
-            menuEffectiveValue();
-            String operation = scrNewValue.nextLine();
-            String[] symbols = operation.split(" ");
-            switch (symbols[0]) {
-                case "1":
-                    result = sum(result, Integer.parseInt(symbols[1]));
-                    break;
-                case "2":
-                    result = sub(result, Integer.parseInt(symbols[1]));
-                    break;
-                case "3":
-                    result = mult(result, Integer.parseInt(symbols[1]));
-                    break;
-                case "4":
-                    result = div(result, Integer.parseInt(symbols[1]));
-                    break;
-                case "5":
-                    result = pow(result, Integer.parseInt(symbols[1]));
-                    break;
-                case "0":
-                    menuPrint();
-                    break;
-                default:
-                    System.out.println("Не известная операция, попробуй еще");
-                    result = Integer.parseInt(symbols[0]) + Integer.parseInt(symbols[2]);
-            }
-
-        }
 
 
-        public static int sum(int a, int b) {
-            return a + b;
-        }
+    public static void sum(int a, int b) {
+        var sum = a + b;
+        System.out.println(sum);
+        result = sum;
+    }
 
-        public static int sub(int a, int b) {
-            return a - b;
-        }
+    public static void sub(int a, int b) {
+        var sub = a - b;
+        System.out.println(sub);
+    }
 
-        public static int mult(int a, int b) {
-            return a * b;
-        }
+    public static void mult(int a, int b) {
+        var mult = a * b;
+        System.out.println(mult);
+        result = mult;
+    }
 
-        public static int div(int a, int b) {
+    public static void div(int a, int b) {
         int div = 0;
         if (b == 0) {
-                System.out.println("На ноль делить нельзя!");
-            } else {
-                div = a / b;
-            }
-            return div;
-        }
-
-        public static int fact(int a) {
-            if (a != 1) {
-                return a * fact(a - 1);
-            } else {
-                return 1;
-            }
-        }
-
-        public static int pow(int a, int b) {
-            return(int)Math.pow(a, b);
-        }
-
-
-
-        public static void exit () {
-            System.out.println("До новых встреч! (:");
-        }
-
-        public static boolean comp(int a, int b) {
-        if (a > b) {
-            return true;
-        } else if ( a < b) {
-            return true;
+            System.out.println("На ноль делить нельзя!");
         } else {
-            return true;
+            div = a / b;
         }
-        }
+        System.out.println(div);
+        result = div;
+    }
 
-        public static void menuEffectiveValue() {
-            System.out.println("1. Сложение");
-            System.out.println("2. Вычитание");
-            System.out.println("3. Умножение");
-            System.out.println("4. Деление");
-            System.out.println("5. Возведение в степень");
-            System.out.println("6. Сравнение");
-            System.out.println("0. Назад");
+    public static int fact(int a) {
+        if (a != 1) {
+            return a * fact(a - 1);
+        } else {
+            return 1;
         }
     }
+
+    public static void pow(int a, int b) {
+        var pow = Math.pow(a, b);
+        System.out.println(pow);
+        result = (int) pow;
+    }
+
+
+    public static void exit() {
+        System.out.println("До новых встреч! (:");
+    }
+
+    public static void comp(String a, String b) {
+        var integerA = Integer.parseInt(a);
+        var integerB = Integer.parseInt(b);
+        if (integerA > integerB) {
+            System.out.printf("%d > %d", integerA, integerB);
+        } else if (integerA < integerB) {
+            System.out.printf("%d < %d", integerA, integerB);
+        } else {
+            System.out.printf("%d == %d",  integerA, integerB);
+            System.out.println();
+        }
+    }
+
+    public static void menuEffectiveValue() {
+        System.out.println("1. Сложение");
+        System.out.println("2. Вычитание");
+        System.out.println("3. Умножение");
+        System.out.println("4. Деление");
+        System.out.println("5. Возведение в степень");
+        System.out.println("6. Сравнение");
+        System.out.println("0. Назад");
+    }
+}
 
